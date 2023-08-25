1c709db0fb12b6519c53be9c850594af651d75a6
Guilherme-Duarte11 <142899834+Guilherme-Duarte11@users.noreply.github.com>
2023-08-25 13:07:25 +0000

    atividades

---
+++ aula 2/exercicios.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char nome[20], numero[20];
+    int idade;
+
+    printf("Informe o seu nome: ");
+    scanf("%s", nome);
+
+    printf("Informe a sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Informe o seu número: ");
+    scanf("%s", numero);
+
+}
